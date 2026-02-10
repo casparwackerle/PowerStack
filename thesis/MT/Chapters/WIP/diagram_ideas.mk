5de86a77e11a221e7796d7310dbe70cd1db32367
Caspar Wackerle <caspar.wackerle@me.com>
2026-02-10 16:48:06 +0100

    clean WIP files

--- thesis/MT/Chapters/WIP/diagram_ideas.mk
+++
@@ -1,8 +0,0 @@
-- diagram to show calibration delay
-- daigram for analysis stages
-- diagram for analysis window selection showing input delay
-- diagram for GPU_corrected
-
-
-other ideas:
-- talk about the overhead created by tycho, and how it's not a lot
